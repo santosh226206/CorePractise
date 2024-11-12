426f85625bd8e8c769211d14b5a3301bf1394345
Shankymoti <shashankkumar648@gmail.com>
2024-11-12 20:49:07 +0530

    changes related to flatmap

--- src/com/practise/javarraylistpractise/FlatenningArraylist.java
+++ src/com/practise/javarraylistpractise/FlatenningArraylist.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class FlatenningArraylist {
     public static void main(String args[]){
         List<List<String>> listOfLists = Arrays.asList(
-                Arrays.asList("Geeks", "For"),
-                Arrays.asList("GeeksForGeeks", "A computer portal"),
-                Arrays.asList("Java", "Programming")
+                Arrays.asList("i", "want"),
+                Arrays.asList("to", "go Delhi at the end of this month"),
+                Arrays.asList("what", "about you")
         );
 
         // Using Stream flatMap(Function mapper)
         listOfLists.stream()
-                .flatMap(list -> list.stream())
+                .flatMap(list -> list.stream()).map(temp->temp.toUpperCase())
                 .forEach(System.out::println);
     }
 }
